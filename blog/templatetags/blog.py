8ada65a9b095081de998eb18c7a0dc45b5cc1b2d
Waldemar Kornewald <wkornewald>
2010-02-09 15:04:05 +0100

    renamed feeds templatetag and changed page to rendered_content

--- blog/templatetags/blog.py
+++ blog/templatetags/blog.py
@@ -4,6 +4,6 @@ from django.template import Library
 register = Library()
 
 @register.inclusion_tag('blog/feeds.html')
-def render_feeds():
+def blog_feeds():
     blogs = Blog.objects.all()
     return {'blogs': blogs}
