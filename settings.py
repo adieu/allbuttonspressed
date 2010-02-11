8650bafe02553b1c4b20ce19be3accd32f706322
Thomas Wanschik <twanschik>
2010-02-11 13:49:57 +0100

    added correct site_id

--- settings.py
+++ settings.py
@@ -16,6 +16,8 @@ ALLOWED_DOMAINS = ('www.allbuttonspressed.com',)
 
 SECRET_KEY = '=r-$b*8hglm+858&9t043hlm6-&6-3d3vfc4((7yd0dbrakhvi'
 
+SITE_ID = 4094
+
 INSTALLED_APPS = (
     'djangoappengine',
     'django.contrib.admin',
