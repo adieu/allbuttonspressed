9283532355ac800699a1c8ea8cf232fc15359576
Waldemar Kornewald <wkornewald>
2010-11-02 13:39:29 +0100

    removed media generator view (middleware is enough)

--- urls.py
+++ urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls.defaults import *
-from django.conf import settings
 from blog.models import PostsSitemap
 
 handler500 = 'djangotoolbox.errorviews.server_error'
@@ -13,8 +12,3 @@ urlpatterns = patterns('',
     (r'^blog/', include('blog.urls')),
     (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
 )
-
-if settings.MEDIA_DEV_MODE:
-    # Generate media on-the-fly
-    from mediagenerator.urls import urlpatterns as mediaurls
-    urlpatterns += mediaurls
