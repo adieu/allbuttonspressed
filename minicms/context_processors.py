17819854d128fb81cec5a7cb261185b8a29d556a
Waldemar Kornewald <wkornewald>
2010-11-23 20:53:22 +0100

    removed unused template variables from context processor

--- minicms/context_processors.py
+++ minicms/context_processors.py
@@ -6,10 +6,7 @@ def cms(request):
 
     return {
         'site_name': settings.SITE_NAME,
-        'site_description': settings.SITE_DESCRIPTION,
         'site_copyright': settings.SITE_COPYRIGHT,
-        'site_base_url': base_url,
-        'twitter_username': getattr(settings, 'TWITTER_USERNAME', None),
         'google_analytics_id': getattr(settings, 'GOOGLE_ANALYTICS_ID', None),
         'google_custom_search_id': getattr(settings, 'GOOGLE_CUSTOM_SEARCH_ID',
                                            None),
